51edb015041c3a16d479d2617b4654b1b8d02a45
Justin Brooks <justin@jzbrooks.com>
2024-10-19 23:21:51 -0400

    Add a null check detector (#43)
    * Add a null check detector
    
    * Tidy
    
    * Add additional tests
    
    * Add a quick fix
    
    * Add a changelog entry
    
    * Fix a lint issue
    
    * Fix tests

---
+++ checks/src/main/kotlin/com/jzbrooks/assertk/lint/checks/AnyAssertionDetector.kt
@@ -0,0 +1,197 @@
+package com.jzbrooks.assertk.lint.checks
+
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.LintFix
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.TextFormat
+import org.jetbrains.uast.UBinaryExpression
+import org.jetbrains.uast.UCallExpression
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.ULiteralExpression
+import org.jetbrains.uast.UQualifiedReferenceExpression
+import org.jetbrains.uast.UastBinaryOperator
+import org.jetbrains.uast.getParentOfType
+import org.jetbrains.uast.isNullLiteral
+import org.jetbrains.uast.skipParenthesizedExprDown
+import java.util.EnumSet
+
+class AnyAssertionDetector :
+    Detector(),
+    Detector.UastScanner {
+    override fun getApplicableUastTypes(): List<Class<out UElement>> =
+        listOf(
+            UCallExpression::class.java,
+        )
+
+    override fun createUastHandler(context: JavaContext) =
+        object : UElementHandler() {
+            private val UBinaryExpression.isNullComparisonExpr: Boolean
+                get() =
+                    operator in setOf(UastBinaryOperator.EQUALS, UastBinaryOperator.NOT_EQUALS) &&
+                        (leftOperand.isNullLiteral() || rightOperand.isNullLiteral())
+
+            override fun visitCallExpression(node: UCallExpression) {
+                if (!node.isKotlin) return
+
+                val method = node.resolve() ?: return
+
+                if (method.isAssertThat) {
+                    val binaryExprArg =
+                        node.valueArguments
+                            .map { it.skipParenthesizedExprDown() }
+                            .filterIsInstance<UBinaryExpression>()
+                    for (argExpr in binaryExprArg) {
+                        if (argExpr.isNullComparisonExpr) {
+                            context.report(
+                                NULL_CHECK_ISSUE,
+                                context.getCallLocation(
+                                    node,
+                                    includeReceiver = false,
+                                    includeArguments = true,
+                                ),
+                                NULL_CHECK_ISSUE.getBriefDescription(TextFormat.TEXT),
+                                getQuickFix(argExpr),
+                            )
+                        }
+                    }
+                }
+            }
+
+            private fun getQuickFix(nullCheckExpr: UBinaryExpression): LintFix? {
+                val assertThatExprReplacement =
+                    when {
+                        (nullCheckExpr.leftOperand as? ULiteralExpression)?.isNull == true -> {
+                            fix()
+                                .replace()
+                                .range(context.getLocation(nullCheckExpr))
+                                .with(nullCheckExpr.rightOperand.sourcePsi!!.text)
+                                .reformat(true)
+                                .build()
+                        }
+
+                        (nullCheckExpr.rightOperand as? ULiteralExpression)?.isNull == true -> {
+                            fix()
+                                .replace()
+                                .range(context.getLocation(nullCheckExpr))
+                                .with(nullCheckExpr.leftOperand.sourcePsi!!.text)
+                                .reformat(true)
+                                .build()
+                        }
+
+                        else -> null
+                    }
+
+                val assertionCall =
+                    nullCheckExpr
+                        .getParentOfType<UQualifiedReferenceExpression>()
+                        ?.selector as? UCallExpression
+
+                val assertionReplacement =
+                    when (nullCheckExpr.operator) {
+                        UastBinaryOperator.EQUALS -> {
+                            assertionCall?.let { call ->
+                                when (call.methodIdentifier?.name) {
+                                    "isTrue" ->
+                                        fix()
+                                            .replace()
+                                            .range(
+                                                context.getCallLocation(
+                                                    call,
+                                                    includeReceiver = false,
+                                                    includeArguments = true,
+                                                ),
+                                            ).with("isNull()")
+                                            .imports("assertk.assertions.isNull")
+                                            .reformat(true)
+                                            .build()
+                                    "isFalse" ->
+                                        fix()
+                                            .replace()
+                                            .range(
+                                                context.getCallLocation(
+                                                    call,
+                                                    includeReceiver = false,
+                                                    includeArguments = true,
+                                                ),
+                                            ).with("isNotNull()")
+                                            .imports("assertk.assertions.isNotNull")
+                                            .reformat(true)
+                                            .build()
+                                    else -> null
+                                }
+                            }
+                        }
+
+                        UastBinaryOperator.NOT_EQUALS -> {
+                            assertionCall?.let { call ->
+                                when (call.methodIdentifier?.name) {
+                                    "isTrue" ->
+                                        fix()
+                                            .replace()
+                                            .range(
+                                                context.getCallLocation(
+                                                    call,
+                                                    includeReceiver = false,
+                                                    includeArguments = true,
+                                                ),
+                                            ).with("isNotNull()")
+                                            .imports("assertk.assertions.isNotNull")
+                                            .reformat(true)
+                                            .build()
+                                    "isFalse" ->
+                                        fix()
+                                            .replace()
+                                            .range(
+                                                context.getCallLocation(
+                                                    call,
+                                                    includeReceiver = false,
+                                                    includeArguments = true,
+                                                ),
+                                            ).with("isNull()")
+                                            .imports("assertk.assertions.isNull")
+                                            .reformat(true)
+                                            .build()
+                                    else -> null
+                                }
+                            }
+                        }
+
+                        else -> null
+                    }
+
+                return if (assertThatExprReplacement != null && assertionReplacement != null) {
+                    fix()
+                        .name("Replace null comparison with null assertion")
+                        .composite(assertThatExprReplacement, assertionReplacement)
+                } else {
+                    null
+                }
+            }
+        }
+
+    companion object {
+        @JvmField
+        val NULL_CHECK_ISSUE: Issue =
+            Issue.create(
+                id = "NullComparisonAssertion",
+                briefDescription = "Use built-in nullability assertions",
+                explanation = """
+                    assertk provides `Assert.isNotNull: Assert` which asserts that the value is not null _and_ transforms the assertion subject into an assertion on the non-null type. It also provides `Assert.isNull` for a similar purpose.
+                """,
+                category = Category.USABILITY,
+                priority = 6,
+                severity = Severity.WARNING,
+                implementation =
+                    Implementation(
+                        AnyAssertionDetector::class.java,
+                        EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES),
+                    ),
+            )
+    }
+}
---
+++ checks/src/test/kotlin/com/jzbrooks/assertk/lint/checks/AnyAssertionDetectorTest.kt
@@ -0,0 +1,206 @@
+package com.jzbrooks.assertk.lint.checks
+
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest
+import com.android.tools.lint.detector.api.Detector
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class AnyAssertionDetectorTest : LintDetectorTest() {
+    override fun getDetector(): Detector = AnyAssertionDetector()
+
+    override fun getIssues() = listOf(AnyAssertionDetector.NULL_CHECK_ISSUE)
+
+    @Test
+    fun `null check in assertThat detected`() {
+        val code =
+            """
+            package clean
+
+            import kotlin.text.ifEmpty
+            import assertk.fail
+            import assertk.assertThat
+            import assertk.assertions.isEqualTo
+            import assertk.assertions.prop
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(name == null).isFalse()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expect(
+            """src/clean/Testing.kt:12: Warning: Use built-in nullability assertions [NullComparisonAssertion]
+        assertThat(name == null).isFalse()
+        ~~~~~~~~~~~~~~~~~~~~~~~~
+0 errors, 1 warnings""",
+        )
+    }
+
+    @Test
+    fun `left operand null check in assertThat detected`() {
+        val code =
+            """
+            package clean
+
+            import kotlin.text.ifEmpty
+            import assertk.fail
+            import assertk.assertThat
+            import assertk.assertions.isEqualTo
+            import assertk.assertions.prop
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(null == name).isFalse()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expect(
+            """src/clean/Testing.kt:12: Warning: Use built-in nullability assertions [NullComparisonAssertion]
+        assertThat(null == name).isFalse()
+        ~~~~~~~~~~~~~~~~~~~~~~~~
+0 errors, 1 warnings""",
+        )
+    }
+
+    @Test
+    fun `not null check in assertThat detected`() {
+        val code =
+            """
+            package clean
+
+            import kotlin.text.ifEmpty
+            import assertk.fail
+            import assertk.assertThat
+            import assertk.assertions.isEqualTo
+            import assertk.assertions.prop
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(name != null).isFalse()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expect(
+            """src/clean/Testing.kt:12: Warning: Use built-in nullability assertions [NullComparisonAssertion]
+        assertThat(name != null).isFalse()
+        ~~~~~~~~~~~~~~~~~~~~~~~~
+0 errors, 1 warnings""",
+        )
+    }
+
+    @Test
+    fun `quick fix applied for not null isTrue`() {
+        val code =
+            """
+            package clean
+
+            import assertk.assertThat
+            import assertk.assertions.isTrue
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(name != null).isTrue()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expectFixDiffs(
+            """Fix for src/clean/Testing.kt line 9: Replace null comparison with null assertion:
+@@ -4 +4
++ import assertk.assertions.isNotNull
+@@ -9 +10
+-         assertThat(name != null).isTrue()
++         assertThat(name).isNotNull()""",
+        )
+    }
+
+    @Test
+    fun `quick fix applied for null isTrue`() {
+        val code =
+            """
+            package clean
+
+            import assertk.assertThat
+            import assertk.assertions.isTrue
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(name == null).isTrue()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expectFixDiffs(
+            """Fix for src/clean/Testing.kt line 9: Replace null comparison with null assertion:
+@@ -4 +4
++ import assertk.assertions.isNull
+@@ -9 +10
+-         assertThat(name == null).isTrue()
++         assertThat(name).isNull()""",
+        )
+    }
+
+    @Test
+    fun `quick fix applied for not null isFalse`() {
+        val code =
+            """
+            package clean
+
+            import assertk.assertThat
+            import assertk.assertions.isFalse
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(name != null).isFalse()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expectFixDiffs(
+            """Fix for src/clean/Testing.kt line 9: Replace null comparison with null assertion:
+@@ -5 +5
++ import assertk.assertions.isNull
+@@ -9 +10
+-         assertThat(name != null).isFalse()
++         assertThat(name).isNull()""",
+        )
+    }
+
+    @Test
+    fun `quick fix applied for null isFalse`() {
+        val code =
+            """
+            package clean
+
+            import assertk.assertThat
+            import assertk.assertions.isFalse
+
+            class Testing {
+                fun nameExists() {
+                    val name: String? = this::class.simpleName
+                    assertThat(name == null).isFalse()
+                }
+            }
+            """.trimIndent()
+
+        lint().files(kotlin(code), *ASSERTK_STUBS).run().expectFixDiffs(
+            """Fix for src/clean/Testing.kt line 9: Replace null comparison with null assertion:
+@@ -5 +5
++ import assertk.assertions.isNotNull
+@@ -9 +10
+-         assertThat(name == null).isFalse()
++         assertThat(name).isNotNull()""",
+        )
+    }
+}
